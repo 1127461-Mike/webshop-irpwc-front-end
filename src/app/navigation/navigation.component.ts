56d51c8eb650ac028734f9aca02c744aa5116d01
Mike Asante <i.hotmep@gmail.com>
2024-01-11 01:15:02 +0100

    added semi auth guard

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
+  constructor(private router: Router) {
+  }
+  toMyOrder(){
+    if (!this.isLoggedIn()) {
+      localStorage.setItem('redirectUrl', this.router.url);
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.router.navigate(['/my-orders'])
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 }
